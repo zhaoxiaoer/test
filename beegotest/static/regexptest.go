30e109cf5f8026ac82f9223c687e7b80fd84b533
zhaoxiaoer <445774833@qq.com>
2017-01-13 19:09:01 +0800

    正则表达式中*可以匹配0次，比较不好理解。判断正则表达式的结果时，首先根据正则表达式将输入字符串拆分为子字符串，然后就容易计算结果了

--- beegotest/static/regexptest.go
+++ beegotest/static/regexptest.go
@@ -89,13 +89,76 @@ func main() {
 	fmt.Println(reg.FindStringSubmatchIndex("hello world"))                          // [0 11 2 4 7 10]
 	fmt.Println(reg.FindReaderSubmatchIndex(bytes.NewReader([]byte("hello world")))) // [0 11 2 4 7 10]
 
-	// 返回b中匹配正则表达式的n个子序列。如果n为-1，则返回所有匹配的子序列
+	// 返回b中匹配正则表达式的不重叠的n个子序列。如果n为-1，则返回所有匹配的子序列
 	// func (re *Regexp) FindAll(b []byte, n int) [][]byte
 	reg = regexp.MustCompile(`he(\w*)o w([[:alpha:]]*)d`)
-	fmt.Println(reg.FindAll([]byte("hello world, heeeeo wwwwd"), -1)) // [[104 101 108 108 111 32 119 111 114 108 100] [104 101 101 101 101 111 32 119 119 119 119 100]]
-	fmt.Println(reg.FindAllString("hello world, heeeeo wwwwd", -1))   // [hello world heeeeo wwwwd]
+	fmt.Println(reg.FindAll([]byte("hello world, heeeeo wwwwd"), -1))      // [[104 101 108 108 111 32 119 111 114 108 100] [104 101 101 101 101 111 32 119 119 119 119 100]]
+	fmt.Println(reg.FindAllString("hello world, heeeeo wwwwd", -1))        // [hello world heeeeo wwwwd]
+	fmt.Println(reg.FindAllIndex([]byte("hello world, heeeeo wwwwd"), -1)) // [[0 11] [13 25]]
+	fmt.Println(reg.FindAllStringIndex("hello world, heeeeo wwwwd", -1))   // [[0 11] [13 25]]
 
-	//
+	// 匹配0到N次
+	reg = regexp.MustCompile("a*")
+	// a*匹配0到N个a，因此a*可以匹配空字符。默认情况下，采用贪婪模式。
+	// 字符串的两端分别有个空字符，所以“abkoaabaccadjjaaae”实际上是"空abkoaabaccadjjaaae空"
+	// 针对“空abkoaabaccadjjaaae空”:
+	// 1 首先从头开始判断字节序列
+	//   当判断完b字符时，即已判断的字节序列为“空ab”时，可以确认第1个匹配的子序列为“空a”，此时a匹配一次，即“a"
+	// 2 剩余的字节序列为“koaabaccadjjaaae空”,头部加上“空",即”空koaabaccadjjaaae空“
+	//   当判断完k字符时，即已判断的字节序列为“空k”时，可以确认第2个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 3 剩余的字节序列为“oaabaccadjjaaae空”,头部加上“空",即”空oaabaccadjjaaae空“
+	//   当判断完o字符时，即已判断的字节序列为“空o”时，可以确认第3个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 4 剩余的字节序列为“aabaccadjjaaae空”,头部加上“空",即”空aabaccadjjaaae空“
+	//   当判断完b字符时，即已判断的字节序列为“空aab”时，可以确认第4个匹配的子序列为“空aa”，此时a匹配2次，即“aa”
+	// 5 剩余的字节序列为“accadjjaaae空”,头部加上“空",即”空accadjjaaae空“
+	//   当判断完c字符时，即已判断的字节序列为“空ac”时，可以确认第5个匹配的子序列为“空a”，此时a匹配1次，即“a”
+	// 6 剩余的字节序列为“cadjjaaae空”,头部加上“空",即”空cadjjaaae空“
+	//   当判断完c字符时，即已判断的字节序列为“空c”时，可以确认第6个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 7 剩余的字节序列为“adjjaaae空”,头部加上“空",即”空adjjaaae空“
+	//   当判断完d字符时，即已判断的字节序列为“空ad”时，可以确认第7个匹配的子序列为“空a”，此时a匹配1次，即“a”
+	// 8 剩余的字节序列为“jjaaae空”,头部加上“空",即”空jjaaae空“
+	//   当判断完j字符时，即已判断的字节序列为“空j”时，可以确认第8个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 9 剩余的字节序列为“jaaae空”,头部加上“空",即”空jaaae空“
+	//   当判断完j字符时，即已判断的字节序列为“空j”时，可以确认第9个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 10 剩余的字节序列为“aaae空”,头部加上“空",即”空aaae空“
+	//   当判断完e字符时，即已判断的字节序列为“空aaae”时，可以确认第10个匹配的子序列为“空aaa”，此时a匹配3次，即“aaa”
+	// 11 剩余的字节序列为“空”
+	//    可以确认第11个匹配的子序列为“空”，此时a匹配0次，即“”
+	// 判断结束。
+	fmt.Printf("%q\n", reg.FindAllString("abkoaabaccadjjaaae", -1)) // ["a" "" "" "aa" "a" "" "a" "" "" "aaa" ""]
+	fmt.Printf("%q\n", reg.FindAllString("bkoaabaccadjjaaae", -1))  // ["" "" "" "aa" "a" "" "a" "" "" "aaa" ""]
+	reg = regexp.MustCompile("a+")
+	fmt.Printf("%q\n", reg.FindAllString("abkoaabaccadjjaaae", -1)) // ["a" "aa" "a" "a" "aaa"]
+	// a{2,}匹配2到N个a。默认情况下，采用贪婪模式。
+	// 字符串的两端分别有个空字符，所以“abkoaabaccadjjaaae”实际上是"空abkoaabaccadjjaaae空"
+	// 针对“空abkoaabaccadjjaaae空”:
+	// 1 首先从头开始判断字节序列
+	//   当已判断的字节序列为“空abkoaab”时，可以确认第1个匹配的子序列为“aa”，此时a匹配2次，即“aa"
+	// 2 剩余的字节序列为“accadjjaaae空”,头部加上“空",即”空accadjjaaae空“
+	//   当已判断的字节序列为“空accadjjaaae”时，可以确认第2个匹配的子序列为“aaa”，此时a匹配3次，即“aaa”
+	// 3 剩余的字节序列为“空”
+	//   不符合正则表达式。
+	// 判断结束。
+	reg = regexp.MustCompile("a{2,}")
+	fmt.Printf("%q\n", reg.FindAllString("abkoaabaccadjjaaae", -1)) // ["aa" "aaa" ]
+
+	// 返回b中匹配正则表达式的不重叠的n个子序列以及（可能有的）分组匹配的子序列
+	// func (re *Regexp) FindAllSubmatch(b []byte, n int) [][][]byte
+	reg = regexp.MustCompile(`he(\w*)o w([[:alpha:]]*)d`)
+	fmt.Printf("%q\n", reg.FindAllSubmatch([]byte("hello world, heeeeo wwwwd"), -1)) // [["hello world" "ll" "orl"] ["heeeeo wwwwd" "eee" "www"]]
+	fmt.Println(reg.FindAllStringSubmatch("hello world, heeeeo wwwwd", -1))          // [[hello world ll orl] [heeeeo wwwwd eee www]]
+	fmt.Println(reg.FindAllSubmatchIndex([]byte("hello world, heeeeo wwwwd"), -1))   // [[0 11 2 4 7 10] [13 25 15 18 21 24]]
+	fmt.Println(reg.FindAllStringSubmatchIndex("hello world, heeeeo wwwwd", -1))     // [[0 11 2 4 7 10] [13 25 15 18 21 24]]
+
+	// 将s中匹配正则表达式的不重叠的子序列作为分隔符，将s分割成n个字符串
+	// func (re *Regexp) Split(s string, n int) []string
+	reg = regexp.MustCompile("a*")                       // 匹配0到N次
+	fmt.Printf("%q\n", reg.Split("abaabaccadddaaae", 5)) // ["" "b" "b" "c" "cadaaae"]
+	// 首先将输入字符串根据正则表达式拆分为子字符串，然后替换即可，注意最开始的空字符串
+	// abaabaccadddaaae 根据判断步骤，拆分为下面的子字符串
+	// 空ab 空aab 空ac 空c 空ad 空d 空d 空aaae 空
+	fmt.Printf("%q\n", reg.Split("abaabaccadddaaae", -1)) // ["" "b" "b" "c" "c" "d" "d" "d" "e"]
+
 	//	r, err := regexp.Compile("(?i:He.*),") // 匹配但不捕获的分组
 	//	if err != nil {
 	//		fmt.Println(err)
