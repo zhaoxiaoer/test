62ec4e72a67e45483ee378c0c69d497dde88aea9
zhaoxiaoer <445774833@qq.com>
2017-03-29 10:41:42 +0800

    优化session测试代码

--- beegotest/sessionCtl/sessionCtl.go
+++ beegotest/sessionCtl/sessionCtl.go
@@ -12,8 +12,7 @@ type SessionCtl struct {
 
 func (sessionCtl *SessionCtl) Get() {
 	fmt.Printf("0 Session GET\n")
-	session := beego.GlobalSessions.SessionStart(sessionCtl.Ctx.ResponseWriter, sessionCtl.Ctx.Request)
-	username := session.Get("username")
+	username := sessionCtl.GetSession("username")
 	sessionCtl.Data["username"] = username
 	fmt.Printf("1 Session GET\n")
 }
@@ -23,8 +22,7 @@ func (sessionCtl *SessionCtl) Post() {
 	username := sessionCtl.Ctx.Request.Form["username"][0]
 	password := sessionCtl.Ctx.Request.Form["password"][0]
 	fmt.Printf("username: %s, password: %s\n", username, password)
-	session := beego.GlobalSessions.SessionStart(sessionCtl.Ctx.ResponseWriter, sessionCtl.Ctx.Request)
-	session.Set("username", username)
-	session.Set("password", password)
+	sessionCtl.SetSession("username", username)
+	sessionCtl.SetSession("password", password)
 	sessionCtl.Redirect("/sessionctl", 301)
 }
